fe8bedb31c7ac1f38c81cfa98d09c385f8b5900f
Dorka <papirfecnike@yahoo.com>
2026-06-06 13:35:31 +0200

    changing prototype

--- app/routes/Prototype.tsx
+++ app/routes/Prototype.tsx
@@ -2,7 +2,7 @@ export default function Prototype() {
   return (
     <div className="prototype">
       <iframe
-        src="https://brown-cafe-82798449.figma.site/"
+        src="https://nimbus-hazy-63781183.figma.site"
         title="No researchers were harmed in the making of this test suite"
         style={{
           width: "100%",
